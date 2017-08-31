d9fc1798b0029a569270d2481d31ccf2772b7cab
Nicolas Adolfo Ansaldi <nansaldi@ws4.labo5>
2017-08-31 19:46:48 -0300

    Empiezo a testear

--- Codigo.cpp
+++ Codigo.cpp
@@ -4,12 +4,12 @@
 
 using namespace std;
 
-int backtracking(vector <pair <int, int> >*, vector<int>*, int, int);
-void Confiable(vector <pair <int, int> >*, vector<int>*, int, int);
-void NoConfiable(vector <pair <int, int> >*, vector<int>*, int, int);
+int backtracking(vector <pair <int, int> >*, vector<int>, int, int);
+void Confiable(vector <pair <int, int> >*, vector<int>, int, int);
+void NoConfiable(vector <pair <int, int> >*, vector<int>, int, int);
 bool VerConfiabilidad(vector <pair <int, int> >*, vector<int>*, int);
-bool aux(vector <pair <int, int> >*, vector <int>*, pair<int, int>); //Funcion que cheque la el pair matchee en el vector <int>*
-bool EnConj(int, vector<int>*); //verifica que el int este en vector<int>
+bool aux(vector <pair <int, int> >*, vector <int>, pair<int, int>); //Funcion que cheque la el pair matchee en el vector <int>*
+bool EnConj(int, vector<int>); //verifica que el int este en vector<int>
 bool modulo(int, int) ;//Dice si dos numeros son iguales en modulo
 
 int res = 0; //variable global que guardara el mejor resultado
@@ -25,7 +25,7 @@ int main(){
 		vector<int> ConjAgentes; //Conj que ira guardando los agentes confiables
 
 		if(agentes > 0 && cantEncuestas >= 0){
-			while(cantEncuestas > 1){ //Me guardo las encuestas en el vector Encuentas
+			while(cantEncuestas > 0){ //Me guardo las encuestas en el vector Encuentas
 				int a;
 				int b;
 				cin >> a;
@@ -33,7 +33,10 @@ int main(){
 				Encuestas.push_back(pair <int,int>(a,b));
 				cantEncuestas--;
 			}
-				cout << backtracking(&Encuestas, &ConjAgentes, 1, agentes)<<endl;
+				backtracking(&Encuestas, ConjAgentes, 1, agentes);
+				cout << res << endl;
+				res = 0;
+				//cout << res << endl;
 		}else{
 			break;
 		}
@@ -41,32 +44,35 @@ int main(){
 	return 0;
 }
 
-int backtracking(vector <pair <int, int> >* encuestas, vector <int>* ConjAgentes, int i, int agentes){
-	if(i >= agentes){
+int backtracking(vector <pair <int, int> >* encuestas, vector <int> ConjAgentes, int i, int agentes){
+	if(i > agentes){	
 		return res;
 	}
 	Confiable(encuestas, ConjAgentes, i, agentes);
 	NoConfiable(encuestas, ConjAgentes, i, agentes);
-	return 0;
 }
 
-void Confiable(vector <pair <int, int> >* Encuestas, vector<int>* ConjAgentes, int i, int agentes){
-	if(i < agentes){
-		if((VerConfiabilidad(Encuestas, ConjAgentes, i))){ //VerConfiabilidad agrega a i al conjAgente si es confiable
-			if(ConjAgentes->size() > res){
-				res = ConjAgentes->size();
+void Confiable(vector <pair <int, int> >* Encuestas, vector<int> ConjAgentes, int i, int agentes){
+	int a = i;
+	a++;
+	if(i <= agentes){
+		if((VerConfiabilidad(Encuestas, &ConjAgentes, i))){ //VerConfiabilidad agrega a i al conjAgente si es confiable
+			if(ConjAgentes.size() > res){
+				res = ConjAgentes.size();
 			}
-			backtracking(Encuestas, ConjAgentes, i++, agentes);
+			backtracking(Encuestas, ConjAgentes, a, agentes);
 		}else{
-			backtracking(Encuestas, ConjAgentes, i++, agentes);
+			backtracking(Encuestas, ConjAgentes, a, agentes);
 		}
 	}
 	return;
 }
 
-void NoConfiable(vector <pair <int, int> >* Encuestas, vector<int>* ConjAgentes, int i, int agentes){
-	if(i < agentes){
-		backtracking(Encuestas, ConjAgentes, i++, agentes);
+void NoConfiable(vector <pair <int, int> >* Encuestas, vector<int> ConjAgentes, int i, int agentes){
+	if(i <= agentes){
+		int a = i;
+		a++;
+		backtracking(Encuestas, ConjAgentes, a, agentes);
 	}
 	return;
 }
@@ -75,40 +81,47 @@ void NoConfiable(vector <pair <int, int> >* Encuestas, vector<int>* ConjAgentes,
 
 bool VerConfiabilidad(vector <pair <int, int> >* Encuestas, vector <int>* ConjAgentes, int i){
 	int n = 0;
-	bool res = true;
+	bool guarda = true;
 	while(n < Encuestas->size()){
 		if((*Encuestas)[n].first == i){
-			res = res && aux(Encuestas, ConjAgentes, (*Encuestas)[n]); //Veo que la encuesta de i matchee con el conjAgente
+			guarda = guarda && aux(Encuestas, *ConjAgentes, (*Encuestas)[n]); //Veo que la encuesta de i matchee con el conjAgente
 		}
-		if(EnConj((*Encuestas)[n].first, ConjAgentes)){
-			res = res && ((*Encuestas)[n].second == i ); //Veo que nadie en el conjunto desconfie de i
+		if(EnConj((*Encuestas)[n].first, *ConjAgentes)){
+			int z = (*Encuestas)[n].second;
+			if(modulo(z, i)){
+				guarda = guarda && ((*Encuestas)[n].second == i ); //Veo que nadie en el conjunto desconfie de i
+			}	
 		}
 		n++;
 	}
-	if(res){
+	if(guarda){
 		ConjAgentes->push_back(i);
 	}
-	return res;
+	return guarda;
 }
 
-bool aux(vector<pair <int, int> >* Encuestas, vector<int>* ConjAgentes, pair<int,int> Ei){
+bool aux(vector<pair <int, int> >* Encuestas, vector<int> ConjAgentes, pair<int,int> Ei){
 	int n = 0;
-	bool res = true;
-	while(n < Encuestas->size()){
+	bool guarda = true;
+	if(Ei.first == (Ei.second*(-1))){
+		return false;
+	}
+	while(n < Encuestas->	size()){
 		if(EnConj((*Encuestas)[n].first, ConjAgentes)){
 			//Caso donde Ei confia o desconfia en alguien, y puede que otro agente ya en el
 			//conjunto de confiables opine distinto
 			if(modulo((*Encuestas)[n].second, Ei.second)){ 
-				res = res && ((*Encuestas)[n].second == Ei.second);
+				guarda = guarda && ((*Encuestas)[n].second == Ei.second);
+				guarda = guarda && VerConfiabilidad(Encuestas, &ConjAgentes, Ei.second);
 			}
 			//Caso donde Ei desconfia en alguien que ya esta en el conjunto
 			if(modulo((*Encuestas)[n].first, Ei.second)){
-				res = res && (Ei.second == (*Encuestas)[n].first);
+				guarda = guarda && (Ei.second == (*Encuestas)[n].first);
 			}
 		}
 		n++;
 	}
-	return res;
+	return guarda;
 }
 
 bool modulo(int a, int b){
@@ -118,13 +131,13 @@ bool modulo(int a, int b){
 	if(b <0){
 		b = (b*(-1));
 	}
-	return a==b;
+	return a==b;	
 }
 
-bool EnConj(int i, vector<int>* ConjAgentes){
+bool EnConj(int i, vector<int> ConjAgentes){
 	int n = 0;
-	while(n < ConjAgentes->size()){
-		if((*ConjAgentes)[n] == i){
+	while(n < ConjAgentes.size()){
+		if(ConjAgentes[n] == i){
 			return true;
 		}
 		n++;
