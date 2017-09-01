612a005bbcc8f6d1f01eff86c7fbd206fb443fa2
Nicolas Adolfo Ansaldi <nansaldi@ws5.labo6>
2017-09-01 16:58:49 -0300

    el buen testeo

--- Codigo.cpp
+++ Codigo.cpp
@@ -11,6 +11,7 @@ bool VerConfiabilidad(vector <pair <int, int> >*, vector<int>*, int);
 bool aux(vector <pair <int, int> >*, vector <int>, pair<int, int>); //Funcion que cheque la el pair matchee en el vector <int>*
 bool EnConj(int, vector<int>); //verifica que el int este en vector<int>
 bool modulo(int, int) ;//Dice si dos numeros son iguales en modulo
+bool ConjValido(vector <int>, vector<pair <int, int> >*); //Chequea que la solucion sea una solucion valida
 
 int res = 0; //variable global que guardara el mejor resultado
 
@@ -45,7 +46,12 @@ int main(){
 }
 
 int backtracking(vector <pair <int, int> >* encuestas, vector <int> ConjAgentes, int i, int agentes){
-	if(i > agentes){	
+	if(i > agentes){
+		if(ConjValido(ConjAgentes, encuestas)){
+			if(ConjAgentes.size() > res){
+				res = ConjAgentes.size();
+			}
+		}	
 		return res;
 	}
 	Confiable(encuestas, ConjAgentes, i, agentes);
@@ -53,16 +59,13 @@ int backtracking(vector <pair <int, int> >* encuestas, vector <int> ConjAgentes,
 }
 
 void Confiable(vector <pair <int, int> >* Encuestas, vector<int> ConjAgentes, int i, int agentes){
-	int a = i;
-	a++;
 	if(i <= agentes){
 		if((VerConfiabilidad(Encuestas, &ConjAgentes, i))){ //VerConfiabilidad agrega a i al conjAgente si es confiable
-			if(ConjAgentes.size() > res){
-				res = ConjAgentes.size();
-			}
+			int a = i;
+			a++;
 			backtracking(Encuestas, ConjAgentes, a, agentes);
 		}else{
-			backtracking(Encuestas, ConjAgentes, a, agentes);
+			NoConfiable(Encuestas, ConjAgentes, i, agentes);
 		}
 	}
 	return;
@@ -82,8 +85,11 @@ void NoConfiable(vector <pair <int, int> >* Encuestas, vector<int> ConjAgentes, 
 bool VerConfiabilidad(vector <pair <int, int> >* Encuestas, vector <int>* ConjAgentes, int i){
 	int n = 0;
 	bool guarda = true;
+	vector<int> ResEncuestas;
 	while(n < Encuestas->size()){
 		if((*Encuestas)[n].first == i){
+			int z = (*Encuestas)[n].second;
+			ResEncuestas.push_back(z);
 			guarda = guarda && aux(Encuestas, *ConjAgentes, (*Encuestas)[n]); //Veo que la encuesta de i matchee con el conjAgente
 		}
 		if(EnConj((*Encuestas)[n].first, *ConjAgentes)){
@@ -91,6 +97,14 @@ bool VerConfiabilidad(vector <pair <int, int> >* Encuestas, vector <int>* ConjAg
 			if(modulo(z, i)){
 				guarda = guarda && ((*Encuestas)[n].second == i ); //Veo que nadie en el conjunto desconfie de i
 			}	
+		}
+		n++;
+	}
+	n = 0;
+	//Chequeo que las encuestas del agente i tenga sentido logico
+	while(n < ResEncuestas.size()){
+		if(EnConj((ResEncuestas[n])*(-1), ResEncuestas)){
+			guarda = false;
 		}
 		n++;
 	}
@@ -103,17 +117,11 @@ bool VerConfiabilidad(vector <pair <int, int> >* Encuestas, vector <int>* ConjAg
 bool aux(vector<pair <int, int> >* Encuestas, vector<int> ConjAgentes, pair<int,int> Ei){
 	int n = 0;
 	bool guarda = true;
-	if(Ei.first == (Ei.second*(-1))){
+	if(Ei.first == (Ei.second *(-1))){ //No confia de si mismo
 		return false;
 	}
-	while(n < Encuestas->	size()){
+	while(n < Encuestas->size()){
 		if(EnConj((*Encuestas)[n].first, ConjAgentes)){
-			//Caso donde Ei confia o desconfia en alguien, y puede que otro agente ya en el
-			//conjunto de confiables opine distinto
-			if(modulo((*Encuestas)[n].second, Ei.second)){ 
-				guarda = guarda && ((*Encuestas)[n].second == Ei.second);
-				guarda = guarda && VerConfiabilidad(Encuestas, &ConjAgentes, Ei.second);
-			}
 			//Caso donde Ei desconfia en alguien que ya esta en el conjunto
 			if(modulo((*Encuestas)[n].first, Ei.second)){
 				guarda = guarda && (Ei.second == (*Encuestas)[n].first);
@@ -143,4 +151,20 @@ bool EnConj(int i, vector<int> ConjAgentes){
 		n++;
 	}
 	return false;
+}
+
+bool ConjValido(vector <int> a, vector<pair <int,int> >* encuestas){
+	int n = 0;
+	bool guarda = true;
+	while(n < encuestas->size()){
+		if(EnConj((*encuestas)[n].first, a)){ //Chequea que si el agente confia en alguien ese alguien este en el conj y si no confia que no este
+			if((*encuestas)[n].second > 0){
+				guarda = guarda && (EnConj((*encuestas)[n].second, a));
+			}else{
+				guarda = guarda && !(EnConj((*encuestas)[n].second, a));
+			}
+		}
+		n++;
+	}
+	return guarda;
 }
