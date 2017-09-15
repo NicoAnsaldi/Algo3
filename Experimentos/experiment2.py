ebac58a2cec0717369708a827ffa0c7e1e19199c
Nicolas Adolfo Ansaldi <nansaldi@ws21.labo7>
2017-09-15 18:28:48 -0300

    sarasa

---
+++ Experimentos/experiment2.py
@@ -0,0 +1,12 @@
+import random
+
+#random.seed(50)
+#encuestas = random.randint(1, 30) #encuestas
+#for agente in range(1,24): #agentes
+#for muestras in range(0,100): #muestras
+	#encuestas = random.randint(1, 31) #encuestas
+for encuestas in range(1,24):
+	print '{0:d} {1:d}'.format(23,encuestas) #agentes y #encuestas
+	for unaencuesta in range(0,encuestas):
+		print '{0:d} {1:s}{2:d}'.format(random.randint(1, 23), random.choice(' -'), random.randint(1, 23))
+print '{0:d} {1:d}'.format(0,0)
